090157f0f92dea4bdc4bba1fdeced8388d9c3167
Thy Nguyen <tee@Thys-iMac.local>
2022-11-16 01:38:54 -0700

    implementing eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -19,3 +19,5 @@ console.log(eqArrays([1, 2, 3], [3, 2, 1]))
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) 
+
+module.exports = { eqArrays }
