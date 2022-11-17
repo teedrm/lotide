33d915abc8236e3a598e162ea136fc6e677a9439
Thy Nguyen <tee@Thys-iMac.local>
2022-11-17 04:02:04 -0700

    implement letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,46 +1,46 @@
 // FUNCTION IMPLEMENTATION
 
 const assertArraysEqual = function(actual, expected) {
-    if (eqArrays(actual, expected)) {
-      console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
-    } else {
-      console.log(`🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-    }
-  };
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
     
-  const eqArrays = function(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    // is array & arrays are not equal
+    if ((Array.isArray(arr1[i]) && Array.isArray(arr2[i])) && (!eqArrays(arr1[i]), arr2[i])) {
+      return false;
+      // arrays not equal
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
-    for (let i = 0; i < arr1.length; i++) {
-      // is array & arrays are not equal
-      if ((Array.isArray(arr1[i]) && Array.isArray(arr2[i])) && (!eqArrays(arr1[i]), arr2[i])) {
-        return false;
-        // arrays not equal
-      } else if (arr1[i] !== arr2[i]) {
-        return false;
+  }
+  return true;
+};
+    
+// ACTUAL FUNCTION
+    
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
       }
     }
-    return true;
-  };
+  }
+  return results;
+};
     
-  // ACTUAL FUNCTION
-    
-  const letterPositions = function(sentence) {
-    const results = {};
-    for (let i = 0; i < sentence.length; i++) {
-      let letter = sentence[i];
-      if (letter !== ' ') {
-        if (results[letter]) {
-          results[letter].push(i);
-        } else {
-          results[letter] = [i];
-        }
-      }
-    }
-    return results;
-  };
-    
-  // TEST
-  console.log(letterPositions('I am Web Developer'));
-  assertArraysEqual(letterPositions("hello").e, [1]);
+// TEST
+console.log(letterPositions('I am Web Developer'));
+assertArraysEqual(letterPositions("hello").e, [1]);
