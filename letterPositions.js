e6e647334f927f5853ef292b05ebd8d998ba5e3c
Thy Nguyen <tee@Thys-iMac.local>
2022-11-17 03:57:05 -0700

    implement letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,45 +1,45 @@
 // FUNCTION IMPLEMENTATION
 
 const assertArraysEqual = function(actual, expected) {
-    if (eqArrays(actual, expected)) {
-      console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
-    } else {
-      console.log(`🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑 Assertion Failed: "${actual}" !== "${expected}"`);
   }
 };
 
-const eqArrays = function (arr1, arr2) {
-    if(arr1.length !== arr2.length) {
-        return false;
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    // is array & arrays are not equal
+    if ((Array.isArray(arr1[i]) && Array.isArray(arr2[i])) && (!eqArrays(arr1[i]), arr2[i])) {
+      return false;
+      // arrays not equal
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
     }
-    for (let i = 0; i < arr1.length; i++) {
-        // is array & arrays are not equal
-        if ((Array.isArray(arr1[i]) && Array.isArray(arr2[i])) && (!eqArrays(arr1[i]), arr2[i])) {
-            return false;
-        // arrays not equal 
-        } else if (arr1[i] !== arr2[i]) {
-            return false
-        }
-    }
-    return true;
-}
+  }
+  return true;
+};
 
 // ACTUAL FUNCTION
 
 const letterPositions = function(sentence) {
-    const results = {};
-      for (let i = 0; i < sentence.length; i++) {
-        if (sentence[i] !== ' ') {
-        if (!results[sentence[i]]) {
-          results[sentence[i]] = [i];
-        } else {
-          results[sentence[i]].push(i);
-        }
-      }        
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      } else {
+        results[sentence[i]].push(i);
+      }
     }
-     return results;
-}
+  }
+  return results;
+};
 
 // TEST
-console.log(letterPositions('I am Web Developer'))
+console.log(letterPositions('I am Web Developer'));
 assertArraysEqual(letterPositions("hello").e, [1]);
