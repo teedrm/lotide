71d5450cbe2a1d190a1147e3ab9775dbd911dead
Thy Nguyen <tee@Thys-iMac.local>
2022-11-21 13:52:36 -0700

    test for tail

---
+++ test/tailTest.js
@@ -0,0 +1,6 @@
+const tail = require('../tail');
+
+assertEqual(tail([5, 6, 7]), [6, 7]);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Lighthouse", "Labs");
+assertEqual(tail([5]), 5);
+assertEqual(tail([]), undefined);
