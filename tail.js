b4ed8c4bbea5565be116ddc67bd8db5b02f6ebd1
Thy Nguyen <tee@Thys-iMac.local>
2022-11-21 21:13:04 -0700

    implement tail

--- tail.js
+++ tail.js
@@ -1,9 +1,5 @@
 const tail = function(arr) {
-  let result = [];
-  for (let i = 1; i < arr.length; i++) {
-    result.push(arr[i]);
-  }
-  return result;
+return arr.slice(1)
 };
 
 module.exports = tail;
