480a588a1d076fb9cf0c30a8fa52fc54ffee022b
Thy Nguyen <tee@Thys-iMac.local>
2022-11-21 19:52:43 -0700

    implement eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -14,9 +14,5 @@ const eqArrays = function (arr1, arr2) {
     }
     return true;
 }
-console.log(eqArrays([1, 2, 3], [1, 2, 3]))
-console.log(eqArrays([1, 2, 3], [3, 2, 1]))
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) 
-
+module.exports = eqArrays;
