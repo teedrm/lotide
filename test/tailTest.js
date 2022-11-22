3853087e22e94f1dab9593047aaf7106c1cb87fd
Thy Nguyen <tee@Thys-iMac.local>
2022-11-21 22:44:29 -0700

    test fail with M&C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,8 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+});
