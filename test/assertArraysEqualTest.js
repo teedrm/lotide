8de562c46f95765df26c520ddbd605fd6f5a0111
Thy Nguyen <tee@Thys-iMac.local>
2022-11-21 20:08:09 -0700

    test file

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual')
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+// TEST CODE
+assertArraysEqual([1,2,3,4],[1,2,3,4]);
+assertArraysEqual([4,3,2,1],[1,2,3,4]);
