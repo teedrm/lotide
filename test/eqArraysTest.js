d7124c4be530cc35380e18776a62a07c28265766
Thy Nguyen <tee@Thys-iMac.local>
2022-11-21 20:01:41 -0700

    test file eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,14 @@
 const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
 
-
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
