af8c3b2bbddc0866f42971ebf9ffc4b1f8d1d386
Thy Nguyen <tee@Thys-iMac.local>
2022-11-21 21:15:41 -0700

    test file

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,10 @@
 const tail = require('../tail');
+const assertEqual = require('../assertEqual')
 
-assertEqual(tail([5, 6, 7]), [6, 7]);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Lighthouse", "Labs");
-assertEqual(tail([5]), 5);
-assertEqual(tail([]), undefined);
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
